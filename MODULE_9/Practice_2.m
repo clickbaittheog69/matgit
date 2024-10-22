598cdab2ee6fcdb490df9c012781678b72001141
Armand B Raichandani <167845694+clickbaittheog69@users.noreply.github.com>
2024-10-22 16:53:26 -0400

    more code

--- MODULE_9/Practice_2.m
+++ MODULE_9/Practice_2.m
@@ -1,11 +1,63 @@
+% clc; clear;
+% el = input('Enter the encrypted letter: ', 's');
+% dl = input('Enter the decryption letter: ','s');
+%
+% el = el + (dl - 97);
+%
+% if el > 122
+%     el = el-26;
+% end
+%
+% fprintf('The decrypted letter is: %s', el)
+
+function decryptedMessage = Practice_2()
 clc; clear;
-el = input('Enter the encrypted letter: ', 's');
-dl = input('Enter the decryption letter: ','s');
 
-el = el + (dl - 97);
+% Input message and key
+message = input('Enter message to decrypt: ', 's');
+key = input('Enter secret key: ', 's');
 
-if el > 122 
-    el = el-26;
+% Validate inputs
+if ~all(isstrprop(message, 'alpha') | isstrprop(message, 'punct') | isspace(message))
+    error('Message can only contain letters, punctuation, and spaces.');
+end
+if ~all(isstrprop(key, 'alpha'))
+    error('Key can only contain letters.');
 end
 
-fprintf('The decrypted letter is: %s', el)
+newkey = '';
+decryptedMessage = '';
+keyLength = length(key);
+keyIndex = 1; % Pointer for the key
+
+for ii = 1:length(message)
+    currentChar = message(ii);
+
+    % Preserve spaces and punctuation
+    if isspace(currentChar) || isstrprop(currentChar, 'punct')
+        decryptedMessage(ii) = currentChar;
+        newkey(ii) = currentChar;
+        continue;
+    end
+
+    % Determine current key character
+    keyChar = upper(key(mod(keyIndex - 1, keyLength) + 1));
+
+    % Decrypt character
+    if isstrprop(currentChar, 'upper')
+        decryptedChar = mod(double(currentChar) - double(keyChar) + 26, 26) + double('A');
+        newkey(ii) = keyChar;
+    else
+        decryptedChar = mod(double(lower(currentChar)) - double(lower(keyChar)) + 26, 26) + double('a');
+        newkey(ii) = lower(keyChar);
+    end
+
+    decryptedMessage(ii) = char(decryptedChar);
+
+    % Move to next key character
+    keyIndex = keyIndex + 1;
+end
+
+fprintf('The new key is: %s\n', newkey);
+fprintf('Decrypted message: %s\n', decryptedMessage);
+end
--- MODULE_9/abr_HW9.m
+++ MODULE_9/abr_HW9.m
@@ -1,39 +1,26 @@
 clc; clear;
-
 message = input('Enter message to decrypt: ', 's');
 key = input('Enter secret key: ','s');
+newkey = message;
 ballschecker = 0;
-for ii = 1:length(message)
-    y = mod(ii-1-ballschecker, length(key))+1;
-    if isstrprop(message(ii), 'upper')
-        message(ii) = upper(key(y));
-    elseif message(ii) == ' '
-        ballschecker = ballschecker +1;
-    elseif isstrprop(message(ii), 'punct')
-        message(ii) = message(ii);
-        ballschecker = ballschecker + 1;
-    else 
-        message(ii) = key(y);
-    end
-end
-fprintf('The new string is %s', message)
-
 for ii = 1:length(message)
     newii = mod(ii-1-ballschecker, length(key)) + 1;
     if message(ii) == ' ' || isstrprop(message(ii),'punct')
         ballschecker = ballschecker + 1;
+        newkey(ii) = message(ii);
     elseif isstrprop(message(ii), 'upper')
         message(ii) = upper(message(ii) - upper((key(newii)) - 97));
+         newkey(ii) = upper(key(newii));
         if message(ii) <65
             message(ii) = message(ii) + 26;
         end
-
     else
         message(ii) = message(ii) - key(newii) + 97;
+        newkey(ii) = key(newii);
         if message(ii) < 97
             message(ii) = message(ii) + 26;
         end
     end
 end
-
+fprintf('Updated key: %s\n', newkey)
 fprintf('Decrypted msg: %s', message)
